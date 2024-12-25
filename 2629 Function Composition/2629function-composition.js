907a2da69e377ee74601bf3f955ea4e61b03a74a
Thomas Mostowfi <thomassmostowfi@gmail.com>
2024-12-25 21:00:05 +0000

    Code challenge submission for question: 2629 Function Composition

---
+++ 2629 Function Composition/2629function-composition.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    return function(x) {
+        let result = x;
+        functions.reverse();
+        functions.forEach((fun) => result = fun(result));
+        return result;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
