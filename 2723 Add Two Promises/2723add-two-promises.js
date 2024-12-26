66d8fa185a78c4aeac42566d94b1741699b24c52
Thomas Mostowfi <thomassmostowfi@gmail.com>
2024-12-26 01:11:52 +0000

    Code challenge submission for question: 2723 Add Two Promises

---
+++ 2723 Add Two Promises/2723add-two-promises.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    //return Promise.all([promise1,promise2])
+    //.then(([result1,result2]) => result1+result2);
+    let res1 = await promise1;
+    let res2 = await promise2;
+    return res1 + res2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
