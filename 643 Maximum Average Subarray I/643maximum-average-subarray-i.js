cfeef4dcfc7e5685c3686d0c27418a2b2f961111
Thomas Mostowfi <thomassmostowfi@gmail.com>
2025-05-07 23:55:59 +0000

    Code challenge submission for question: 643 Maximum Average Subarray I

--- 643 Maximum Average Subarray I/643maximum-average-subarray-i.js
+++ 643 Maximum Average Subarray I/643maximum-average-subarray-i.js
@@ -4,15 +4,16 @@
  * @return {number}
  */
 var findMaxAverage = function (nums, k) {
-    let localAmount = nums.slice(0, k).reduce((acc, curr) => {
-        return acc += curr;
-    })
-    let maxAmount = localAmount;
-
-    for (let i = k; i < nums.length; i++) {
-        localAmount = localAmount - nums[i - k] + nums[i];
-        localAmount > maxAmount && (maxAmount = localAmount);
+    let left = 0;
+    let sum = 0;
+    let max = 0;
+    for(let right = 0; right < nums.length; right++){
+        sum += nums[right];
+        while(right - left + 1 == k){
+            sum > max && (max = sum);
+            sum -= nums[left];
+            left++;
+        }
     }
-
-    return localAmount > maxAmount ? localAmount/k : maxAmount/k;
+    return max/k;
 };
