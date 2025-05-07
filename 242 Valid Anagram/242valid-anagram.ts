e6dd4e294da6aa6e6134fdaa6a672693f6f4a363
Thomas Mostowfi <thomassmostowfi@gmail.com>
2025-05-07 23:20:56 +0000

    Code challenge submission for question: 242 Valid Anagram

--- 242 Valid Anagram/242valid-anagram.ts
+++ 242 Valid Anagram/242valid-anagram.ts
@@ -1,3 +1,12 @@
 function isAnagram(s: string, t: string): boolean {
-    return s.split('').sort().join('') === t.split('').sort().join('');
+    if (s.length !== t.length) return false;
+
+    const count = new Array(26).fill(0); // alphabet minuscule
+
+    for (let i = 0; i < s.length; i++) {
+        count[s.charCodeAt(i) - 97]++; // 'a' = 97
+        count[t.charCodeAt(i) - 97]--;
+    }
+
+    return count.every(c => c === 0);
 };
