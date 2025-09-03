32ef8a146d72b64c02dff0a8ad8810d2047fe153
Thomas Mostowfi <thomassmostowfi@gmail.com>
2025-09-03 15:07:25 +0000

    Code challenge submission for question: 231 Power of Two

---
+++ 231 Power of Two/231power-of-two.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if(n < 1) return false;
+    else if(n == 1) return true;
+    else return isPowerOfTwo(n/2);
+};
